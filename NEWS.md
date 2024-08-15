3a29a2516cc0dfe12e8687f7ef7077f3c50e9f9a
Finn Lindgren <Finn.Lindgren@gmail.com>
2024-08-15 18:58:47 +0100

    Fix memory issue in inla.mesh3d.fem

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 # inlamesh3d (development version)
 
 * Updated documentation.
+* Fixed excessive memory usage in the `inla.mesh3d.fem` method. (version `0.0.0.9003`)
 
-# inlamesh3d 0.0.0.9001
+# inlamesh3d 0.0.0
 
 * Initial experimental version.
--- R/mesh3d.R
+++ R/mesh3d.R
@@ -164,7 +164,13 @@ inla.mesh3d.fem <- function(mesh, ...) {
     dims = c(mesh$n, mesh$n)
   )
 
-  list(c0 = c0, g1 = g1, g2 = g1 %*% (g1 / vols_v), va = vols_v, vt = vols_t)
+  list(
+    c0 = c0,
+    g1 = g1,
+    g2 = g1 %*% Matrix::Diagonal(mesh$n, 1 / vols_v) %*% g1,
+    va = vols_v,
+    vt = vols_t
+  )
 }
 
 
